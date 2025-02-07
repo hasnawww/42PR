aa6f830c92c5e506f61c12839d62cee4c23d78a6
hasnawww <iliane.hsn@outlook.fr>
2025-02-07 17:53:06 +0000

    so longanh

--- 42pr/so_long/parsing.c
+++ 42pr/so_long/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hasnawww <hasnawww@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:20:50 by hasnawww          #+#    #+#             */
-/*   Updated: 2025/02/06 18:36:11 by hasnawww         ###   ########.fr       */
+/*   Updated: 2025/02/07 17:51:51 by hasnawww         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ int	dbstrlen(int fd)
 		i++;
 		free(line);
 	}
+	close(fd);
 	return (i);
 }
 
@@ -91,58 +92,77 @@ int	check_border(char *line)
 // 	return(0);
 // }
 
-int	main(int ac, char **av)
+void	ft_error()
 {
-	int 	i;
-	int		fd;
-	char	**lines;
-	int		size;
+	perror("ERROR MALLOC");
+	exit(0);
+}
+
+void	get_map(char **av, char ** lines)
+{
+	int	i;
+	int	fd;
+	int	size;
 
 	i = 0;
 	fd = open(av[1], O_RDONLY);
 	size = dbstrlen(fd);
-	close(fd);
 	fd = open(av[1], O_RDONLY);
-	lines = malloc(sizeof(char) * size);
-	if (!lines)
+	lines = malloc(sizeof(char *) * size);
+	if (!lines || !fd)
 	{
-		printf("ojui");
-		perror("ERROR MALLOC");
-		exit(0);
+		if (!lines)
+			free(lines);
+		ft_error();
 	}
-	if (fd == -1)
+	if (is_ber(av[1]))
 	{
-		perror("ERROR FD");
-		exit(0);
+		while(i < size)
+		{
+			lines[i] = get_next_line(fd);
+			i++;
+		}
 	}
+	int j = 0;
+	while (j < i) 
+	{
+	printf("j = %d\n", j);
+	printf("%s", lines[3]);
+	j++;
+	}
+	close(fd);
+}
+
+int	parsing(char **lines, char **av, int ac)
+{
+	int	fd;
+
 	if (ac == 2)
 	{
+		fd = open(av[1], O_RDONLY);
+		get_map(av, lines);
 		if (is_ber(av[1]))
 		{
-				while(i < size){
-				lines[i] = get_next_line(fd);
-				i++;
-				}
-				// printf("%s\n", lines[i]);
-				int j = 0;
-				while (j < i) {
-				printf("%s", lines[j]);
-				j++;
-				}
-				printf("\n");
-				// free(lines[i]);
-		}
-			if (check_border(lines[0]) && check_border(lines[i - 1]))
+			if (check_border(lines[0]) && check_border(lines[dbstrlen(fd) - 1]))
 			{
-				printf("%d\n", i);
-				printf("elleveutelleveut\n");
-			}
-			else
-			{
-				printf("elle veut grv pas bahahhaha\n");
+				printf("hatem ben arfa > messi");
 			}
 		}
-	free(lines);
-	return(0);
 	}
+	return(0);
+}
+
+int	main(int ac, char **av)
+{
+	// int 	i;
+	char	**lines;
+
+	// i = 0;
+	lines = NULL;
+	if (ac == 2)
+	{
+		parsing(lines, av, ac);
+	}
+	return(0);
+}
 // }
