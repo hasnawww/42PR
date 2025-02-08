04ef714a49bc078287fe007b05752f4e4e4aa459
Iliane Hasnaoui <ilhasnao@c4r2p3.42nice.fr>
2025-02-08 20:34:29 +0100

    leaks sur leaks sur leaks

--- 42pr/so_long/parsing.c
+++ 42pr/so_long/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hasnawww <hasnawww@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ilhasnao <ilhasnao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:20:50 by hasnawww          #+#    #+#             */
-/*   Updated: 2025/02/07 17:51:51 by hasnawww         ###   ########.fr       */
+/*   Updated: 2025/02/08 20:29:57 by ilhasnao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ int	is_ber(char *av)
 		return (0);
 }
 
+// PROBLEME AVEC COUNT LINES ET TT CA
+
 int	dbstrlen(int fd)
 {
 	int		i;
@@ -44,8 +46,28 @@ int	dbstrlen(int fd)
 		i++;
 		free(line);
 	}
-	close(fd);
 	return (i);
+}
+
+int count_lines(const char *filename)
+{
+    int     fd;
+    int     line_count;
+    char    *line;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return -1;  // Error opening file
+
+    line_count = 0;
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        line_count++;
+        free(line);
+    }
+    close(fd);
+
+    return line_count;
 }
 
 int	check_border(char *line)
@@ -57,8 +79,6 @@ int	check_border(char *line)
 	i = 0;
 	while (line[i] && line[i] != '0')
 	{
-		// if (line[i] == '0')
-		// 	return (0);
 		i++;
 	}
 	if (line[i] == '0')
@@ -66,88 +86,65 @@ int	check_border(char *line)
 	return (1);
 }
 
-// int	main(int ac, char **av)
-// {
-// 	// int	i;
-// 	int	fd;
-// 	char	*lines = "11111";
-
-// 	// i = 0;
-// 	if (ac == 2){
-//  	fd = open(av[1], O_RDONLY);
-// 	if (!fd)
-// 	{
-// 		perror("error");
-// 		exit(0);
-// 	}
-// 	if (check_border(lines))
-// 	{
-// 		printf("ouioui");
-// 	}
-// 	else
-// 	{
-// 		printf("non");
-// 	}
-// 	}
-// 	return(0);
-// }
-
 void	ft_error()
 {
 	perror("ERROR MALLOC");
 	exit(0);
 }
 
-void	get_map(char **av, char ** lines)
+char **get_map(char *filename)
 {
-	int	i;
-	int	fd;
-	int	size;
+	int fd;
+	char **lines;
+	int line_count = 0;
+	char *line;
 
-	i = 0;
-	fd = open(av[1], O_RDONLY);
-	size = dbstrlen(fd);
-	fd = open(av[1], O_RDONLY);
-	lines = malloc(sizeof(char *) * size);
-	if (!lines || !fd)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1 || !is_ber(filename))
 	{
-		if (!lines)
-			free(lines);
 		ft_error();
 	}
-	if (is_ber(av[1]))
+
+	lines = malloc(sizeof(char *) * 1000); // Still using a fixed size, consider dynamic allocation
+	if (!lines)
 	{
-		while(i < size)
+		ft_error();
+		close(fd);
+		return NULL;
+	}
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		lines[line_count++] = line;
+		ft_putstr_fd(line, 1);
+		if (line_count >= 1000) // Safety check
 		{
-			lines[i] = get_next_line(fd);
-			i++;
+			ft_error();
+			break;
 		}
 	}
-	int j = 0;
-	while (j < i) 
-	{
-	printf("j = %d\n", j);
-	printf("%s", lines[3]);
-	j++;
-	}
+	lines[line_count] = NULL; // Null-terminate the array
 	close(fd);
+	return lines;
 }
 
-int	parsing(char **lines, char **av, int ac)
+int	parsing(char **av, int ac)
 {
 	int	fd;
+	char **map;
 
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
-		get_map(av, lines);
+		map = get_map(av[1]);
 		if (is_ber(av[1]))
 		{
-			if (check_border(lines[0]) && check_border(lines[dbstrlen(fd) - 1]))
+			if (check_border(map[0]) && check_border(map[3]))
 			{
-				printf("hatem ben arfa > messi");
+				ft_putstr_fd("oui\n", 1);
 			}
 		}
+		free(map);
 	}
 	return(0);
 }
@@ -155,14 +152,10 @@ int	parsing(char **lines, char **av, int ac)
 int	main(int ac, char **av)
 {
 	// int 	i;
-	char	**lines;
 
-	// i = 0;
-	lines = NULL;
 	if (ac == 2)
 	{
-		parsing(lines, av, ac);
+		parsing(av, ac);
 	}
 	return(0);
 }
-// }
